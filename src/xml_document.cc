1cac1618e9792e07eafae8e075e0521912ecac52
Wu Yuntao <wyt.brandon@gmail.com>
2012-05-18 23:00:30 +0800

    Update recovery parse option.

--- src/xml_document.cc
+++ src/xml_document.cc
@@ -95,7 +95,7 @@ XmlDocument::ToString(const v8::Arguments& args)
 
     xmlChar* buffer = NULL;
     int len = 0;
-    xmlDocDumpFormatMemoryEnc(document->xml_obj, &buffer, &len, "UTF-8", 0);
+    xmlDocDumpFormatMemoryEnc(document->xml_obj, &buffer, &len, "UTF-8", 32);
     v8::Local<v8::String> str = v8::String::New((const char*)buffer, len);
     xmlFree(buffer);
 
